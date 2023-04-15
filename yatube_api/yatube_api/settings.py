8191391aa4266aa43282ed33381fec23f3c2625c
Олег Кирьянов <xagatgx@yandex.ru>
2023-04-15 13:14:34 +0300

    fix due to PEP8, delete unuseful files

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -100,8 +100,8 @@ REST_FRAMEWORK = {
     ],
 }
 SIMPLE_JWT = {
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=3),
-   'AUTH_HEADER_TYPES': ('Bearer',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=3),
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
